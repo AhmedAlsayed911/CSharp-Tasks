cac0366ad5163a8f613769b5308a5e903368d75d
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-02-22 21:20:19 +0200

    add max distance between similar numbers task

---
+++ MaxDistanceBetweenSimilarNumbers/MaxDistanceBetweenSimilarNumbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace MaxDistanceBetweenSimilarNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            Console.Write("Enter the size of array : ");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+
+            for(int i=0; i<size; i++)
+            {
+                Console.Write($"Enter item #{i + 1} : ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maxDistance = 0;
+            for(int i=0; i<size; i++)
+            {
+                for(int j = i+1;j<size; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        if ((j - i) > maxDistance)
+                            maxDistance = j - i - 1;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Max Distance : {maxDistance}");
+
+            sw.Stop();
+
+            Console.WriteLine($"Task takes {sw.ElapsedMilliseconds} Milliseconds");
+        }
+    }
+}

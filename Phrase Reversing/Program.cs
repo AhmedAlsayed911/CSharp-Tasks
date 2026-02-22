4fb085918b20a4f9be0445b4833d533107e5a6e7
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-02-22 21:20:19 +0200

    add phrase reversing task

---
+++ Phrase Reversing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace Phrase_Reversing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var str = Console.ReadLine();
+            string[] splitedStr = str.Split(' ');
+            Array.Reverse(splitedStr);
+            Console.WriteLine(string.Join(" ", splitedStr));
+            sw.Stop();
+        }
+    }
+}

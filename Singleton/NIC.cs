8e564b943dd10a784bd9f0ed91d8a209a6474927
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-02-22 21:20:20 +0200

    add singleton task

---
+++ Singleton/NIC.cs
@@ -0,0 +1,27 @@
+﻿namespace Singleton
+{
+    internal class NIC
+    {
+        public string Manufacture { get; }
+        public string MacAddress { get; }
+        public string Type { get; }
+
+        private static NIC instance;
+
+        private NIC(string manufacture, string macAddress, string type)
+        {
+            Manufacture = manufacture;
+            MacAddress = macAddress;
+            Type = type;
+        }
+
+        public static NIC GetInstance(string manufacture, string macAddress, string type)
+        {
+            if (instance is null)
+                instance = new NIC(manufacture, macAddress, type);
+
+            return instance;
+        }
+
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = NIC.GetInstance("test", "255.255.255.0", "testing");
+            Console.WriteLine(n.ToString());
+        }
+    }
+}

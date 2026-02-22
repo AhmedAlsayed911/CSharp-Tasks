b4e35f9d4f591febdb5468d64092c3ea090d2553
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-02-22 21:20:18 +0200

    add from one to sky V2 task

---
+++ FromOneToSky-V2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace FromOneToSky_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            int x;
+            int counter = 0;
+            for (int i = 1; i < 99999999; i++) 
+            {
+                x = i;
+                while(x > 0)
+                {
+                    if(x % 10 == 1)
+                        counter++;
+
+                    x /= 10;
+                }
+            }
+            sw.Stop();
+
+            Console.WriteLine($"it has {counter} ones");
+            Console.WriteLine($"Task takes {sw.ElapsedMilliseconds} Milliseconds");
+        }
+    }
+}

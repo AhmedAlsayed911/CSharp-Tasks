feb2d3e3b1b9391b42cc8bf8b258b76cbec4ba19
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-02-22 21:20:21 +0200

    add swap me task

---
+++ SwapMe/SwapMe/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SwapMe
+{
+    internal class Program
+    {
+        public static void Swap<T>(ref T x , ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+        static void Main(string[] args)
+        {
+            int x = 5, y = 6;
+            Console.WriteLine($"Before Swap x = {x} , y = {y}");
+            Swap(ref x, ref y);
+            Console.WriteLine($"After  Swap x = {x} , y = {y}");
+        }
+    }
+}

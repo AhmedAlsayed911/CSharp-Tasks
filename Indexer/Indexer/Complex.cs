2bd9730f7fca4861f09800c4baf66108d466243a
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-02-22 21:20:19 +0200

    add indexer task

---
+++ Indexer/Indexer/Complex.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indexer
+{
+    internal class Complex
+    {
+        int _real;
+        int _img;
+
+        public Complex(int real, int img)
+        {
+            _real = real;
+            _img = img;
+        }
+
+        public Complex(int real) : this(real, 9)
+        {
+
+        }
+
+        public Complex():this(0,0)
+        {
+
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index == 0) return _real;
+                if (index == 1) return _img;
+                else throw new IndexOutOfRangeException();
+            }
+
+            set
+            {
+                if (index == 0) _real = value;
+                else if (index == 1) _img = value;
+                else throw new IndexOutOfRangeException();
+            }
+        }
+
+        public override string ToString() => $"{_real}+{_img}J";
+
+        public int Real
+        {
+            get => _real;
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("Real number connot be Minus !!");
+                else
+                    _real = value;
+            }
+        }
+    }
+}
---
+++ Indexer/Indexer/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Complex(5);
+
+            c[0] = 4;
+            c[1] = 10;
+
+            Console.Write(c.ToString());
+        }
+    }
+}

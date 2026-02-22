199c2e734ae632acad7ed88a8a090e373863cee1
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-02-22 21:20:20 +0200

    add point handler task

---
+++ PointHandler/PointHandler/Point.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointHandler
+{
+    internal class Point : IComparable, ICloneable
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Z { get; private set; }
+
+        public Point(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Point(int x, int y) : this(x, y, 0)
+        {
+
+        }
+
+        public Point(int x) : this(x, 0, 0)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Point Coordinates :  ({X}, {Y}, {Z})";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not null)
+            {
+                Point p = (Point)obj;
+                return X == p.X && Y == p.Y && Z == p.Z;
+            }
+            return false;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is null || obj is not Point p) return -1;
+
+            int cmp = X.CompareTo(p.X);
+            if (cmp != 0) return cmp;
+
+            int cmp2 = Y.CompareTo(p.Y);
+            if (cmp2 != 0) return cmp2;
+
+            return Z.CompareTo(p.Z);
+        }
+
+
+        public object Clone()
+        {
+            return new Point(X, Y, Z);
+        }
+    }
+}
---
+++ PointHandler/PointHandler/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PointHandler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Point(1);
+            //Console.WriteLine(p.ToString());
+
+            //string s = p; // cannot Implicitly convert 3D Point to string Type
+            //string s = (string)p; // cannot Explicitly convert 3D Point to string Type
+
+            //Point[] points = new Point[2];
+
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine($"Enter Point {i + 1}:");
+
+            //        int x = int.Parse(Console.ReadLine());
+            //        int y = int.Parse(Console.ReadLine());
+            //        int z = int.Parse(Console.ReadLine());
+
+            //        points[i] = new Point(x, y, z);
+            //    }
+            //    catch(Exception e)
+            //    {
+            //        Console.WriteLine(e.Message);
+            //    }
+
+            //}
+
+            //int i = 0;
+            //Point[] Points2 = new Point[2];
+
+            //do
+            //{
+            //    Console.WriteLine($"Enter Dimensions of Point {i + 1}:");
+
+            //    Console.Write("X: ");
+            //    bool okX = int.TryParse(Console.ReadLine(), out int x);
+
+            //    Console.Write("Y: ");
+            //    bool okY = int.TryParse(Console.ReadLine(), out int y);
+
+            //    Console.Write("Z: ");
+            //    bool okZ = int.TryParse(Console.ReadLine(), out int z);
+
+            //    if (!(okX && okY && okZ))
+            //    {
+            //        Console.WriteLine("Invalid input! Please enter integers only.\n");
+            //        continue;
+            //    }
+
+            //    Points2[i] = new Point(x, y, z);
+            //    i++;
+
+            //} while (i < 2);
+
+            //Console.WriteLine(Points2[0].ToString());
+
+            //var points3 = new Point[2];
+            //int x, y, z;
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    try
+            //    {
+            //        Console.Write($"Enter x of Point {i + 1} : ");
+            //        x = Convert.ToInt32(Console.ReadLine());
+
+            //        Console.Write($"Enter y of Point {i + 1} : ");
+            //        y = Convert.ToInt32(Console.ReadLine()); ;
+
+            //        Console.Write($"Enter z of Point {i + 1} : ");
+            //        z = Convert.ToInt32(Console.ReadLine());
+
+            //        points3[i] = new Point(x, y, z);
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine($"{e.Message}");
+            //    }
+            //}
+
+            //Console.WriteLine(points3[0].ToString());
+
+            //Console.WriteLine(Points2[0] == Points2[1]); // false ref != ref.
+            //Console.WriteLine(Points2[0].Equals(Points2[1])); // true value = value.
+
+            var points = new Point[5] {new Point(1,4,3)
+                , new Point(1, 2, 3)
+                , new Point(0, 2, 3)
+                , new Point(8, 2, 3)
+                , new Point(9, 2, 3) };
+
+            Array.Sort(points);
+
+            foreach (var itme in points)
+                Console.WriteLine(itme.ToString());
+        }
+    }
+}

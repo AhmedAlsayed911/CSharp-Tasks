f4fe4c07e016fe4d8dbbfc2b9b411b43b76b396e
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-02-22 21:20:20 +0200

    add math handler task

---
+++ MathHandler/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathHandler
+{
+    internal static class Math
+    {
+        public static int Add (int x , int y) => x + y;
+
+        public static int Sub(int x, int y) => x - y;
+
+        public static int Mul(int x, int y) => x * y;
+
+        public static double Div(double x , double y) => x / y;
+
+    }
+}
